d26093094a95fd2a4f6bac3c01e299227dc8b253
AJ <businessphone92@gmail.com>
2019-12-12 13:09:43 -0800

    equals

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var tenPercentBtn = document.getElementsByClassName('tenPercent');
 var tfifteenPercentBtn = document.getElementsByClassName('fifteenPercent');
 var twentyPercentBtn = document.getElementsByClassName('twentyPercent');
 var bill = document.getElementById('billInput');
-var tip = document.getElementById('tipAmount');
+var tipEquals = document.getElementById('tipAmount');
 
 
 function calcTen()
 {
     var billInput = bill.value;
     var tipAmount = billinput * .1;
-    tip.innerHTML = '10% tip is $' + tipAmount;
+    tipEquals.innerHTML = '10% tip is $' + tipAmount;
 }
 
 
