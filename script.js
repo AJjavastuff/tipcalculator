6728a3f879dbe58b8f08f21a6220199049994311
AJ <businessphone92@gmail.com>
2019-12-12 14:52:31 -0800

    not working commit

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-var tenPercentBtn = document.querySelector('tenPercent');
-// var tfifteenPercentBtn = document.getElementsByClassName('fifteenPercent');
-// var twentyPercentBtn = document.getElementsByClassName('twentyPercent');
-var bill = document.getElementById('billInput');
-var tipEquals = document.getElementById('tipAmount');
+// var tenPerBtn = document.getElementById("tenPercent");
+// // var tfifteenPerBtn = document.getElementById("fifteenPercent");
+// // var twentyPerBtn = document.getElementById("twentyPercent");
+// var bill = document.getElementById('billInput');
+// var tipEquals = document.getElementById("tipAmount");
 
 
-function calcTen()
-{
-    var billInput = bill.value;
-    var tipAmount = billinput * .1;
-    tipEquals.innerHTML = '10% tip is $' + tipAmount;
+
+// function calculateTen()
+// {
+//     var billInput = bill.value;
+//     var tipAmount = billInput * .1;
+//     tipAmount.innerHTML = '10% tip is $' + tipAmount;
+// }
+
+// tenPerBtn.addEventListener("click", calcTen);
+// document.getElementsByClassName('tenPerBtn').addEventListener('click', calcTen());
+
+window.onload = function() {
+    $("calculateButton").onclick = ClickHandler;
+};
+
+var $ = function(id) {
+    return document.getElementById(id);
 }
 
 
-document.getElementById(tenPercentBtn).addEventListener('click', calcTen())
+function ClickHandler() {
+    var billAmount = parseFloat($("billAmount").value);
+    var tipPercentage = parseFloat($("serviceQuality").value);
+    var numOfPeople = parseInt($("numOfPeople").value);
+
+    var tipAmount = tipPercentage * billAmount;
+    var totalAmount = tipAmount + billAmount;
+    var perPersonTotal = totalAmount / numOfPeople;
+
+    $("resultsParagraph").innerHTML =
+    "Per person total: $" + perPersonTotal;
+}
